c21dbc2970188480f6560cbbd88a878abd34e663
yatheesh3333 <yatheeshreddy3333@gmail.com>
2020-01-08 17:13:13 +0530

    0513

---
+++ Test Suites/LOGIN/Login_Valid and Invalid.ts
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login_Valid and Invalid</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>230</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>799a4984-160f-4f97-8513-c973e8d5b44e</testSuiteGuid>
+   <testCaseLink>
+      <guid>eca70552-e8dc-46af-9fa9-7550c8d36f2e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Atlas Platform/Platform Navigation/Login/Valid_Login</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>3a269d68-4c3d-422c-8b16-5b3d096cd8ab</id>
+         <iterationEntity>
+            <iterationType>RANGE</iterationType>
+            <value>1-16</value>
+         </iterationEntity>
+         <testDataId>Data Files/AMP_Y</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>3a269d68-4c3d-422c-8b16-5b3d096cd8ab</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Email</value>
+         <variableId>b3b2104a-c26f-42a1-9794-dda27fc68d0a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>3a269d68-4c3d-422c-8b16-5b3d096cd8ab</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>47b7ce68-91d4-485c-aba9-31c7cbb43c65</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>8223df05-ee91-4934-bff1-33b5c601100c</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>04ee0d63-a2ea-45af-971b-bddf7846664c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Atlas Platform/Platform Navigation/Login/InValid Email ID_Login</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>bf8bdddd-7cab-4c1e-bd4b-53e566a935e4</id>
+         <iterationEntity>
+            <iterationType>RANGE</iterationType>
+            <value>19-38</value>
+         </iterationEntity>
+         <testDataId>Data Files/AMP_Y</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>bf8bdddd-7cab-4c1e-bd4b-53e566a935e4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Email</value>
+         <variableId>0e64ab1a-2e7e-4fb8-a0c1-445e91930d90</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>bf8bdddd-7cab-4c1e-bd4b-53e566a935e4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>da2d6be7-b1e3-480a-b78b-ff4ddf997f96</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8aa90486-5253-4057-ac00-47994c5742fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Atlas Platform/Platform Navigation/Login/InValid Pasword_Login</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>3de4702d-a6a4-4809-8c9f-802158e776b1</id>
+         <iterationEntity>
+            <iterationType>RANGE</iterationType>
+            <value>40-54</value>
+         </iterationEntity>
+         <testDataId>Data Files/AMP_Y</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>3de4702d-a6a4-4809-8c9f-802158e776b1</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Email</value>
+         <variableId>87a1705d-bfaa-4414-a832-e4d39b0964e7</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>3de4702d-a6a4-4809-8c9f-802158e776b1</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>6f7b3bb8-9f36-49d3-b444-29f2b74f9fcd</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
