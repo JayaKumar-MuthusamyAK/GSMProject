f1aaf88edd7088093b61e674a68bfaa64018967e
yatheesh3333 <yatheeshreddy3333@gmail.com>
2020-01-21 10:30:10 +0530

    21-01-20 commit 1

---
+++ Object Repository/Login_Page/Page_/Error Message_Email Address-Required Field.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Error Message_Email Address-Required Field</name>
+   <tag></tag>
+   <elementGuidId>ad8c9247-ad46-4b5d-a828-b0712473c664</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[text()='Required Field']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//span[text()='Required field']</value>
+   </webElementProperties>
+</WebElementEntity>
